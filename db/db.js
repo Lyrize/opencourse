1754dd2416fc1c5255b40b4327857f1de25bae5a
Lyrize <83056661+Lyrize@users.noreply.github.com>
2025-07-26 19:59:32 +0700

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,18 +1,23 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+// Load .env file (untuk pemakaian lokal)
+dotenv.config();
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "railway",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT, // tambahkan port
 });
 
 connection.connect((err) => {
   if (err) {
-    throw err;
-  } else {
-    console.log("Database Connected.");
+    console.error("Database connection failed:", err.stack);
+    return;
   }
+  console.log("Database Connected.");
 });
 
 export default connection;
